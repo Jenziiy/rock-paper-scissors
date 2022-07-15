a5f2226c11f6d261af4d30ee19b98a477e0bb601
user.email <developerjenzii@gmail.com>
2022-07-15 17:30:35 +0200

    Press any button to return its selection value.

--- javascript.js
+++ javascript.js
@@ -1,24 +1,21 @@
 let counter = 0;
 let winCount = 0;
 
-const rock = document.querySelector('#rock');
-const paper = document.querySelector('#paper');
-const scissors = document.querySelector('#scissors');
-
-
 function game(){
+
       const gameInput = ['rock','paper', 'scissors'];
-      const playerSelection = prompt(`${gameInput[0]}, ${gameInput[1]} or ${gameInput[2]} ?`,`${gameInput[0]}`).toLowerCase();
-      const computerSelection = computerPlay().toLowerCase();
-      const gameOutcome = score => alert(gameOutput[score])
-      const gameOutput = ['you won!', 'you lost :( ', 'it\'s a tie!!!!'];
+      //const playerSelection = prompt(`${gameInput[0]}, ${gameInput[1]} or ${gameInput[2]} ?`,`${gameInput[0]}`).toLowerCase();
+      
+      function getPlayerChoice () {
+       document.querySelectorAll('button').forEach(btn => btn.addEventListener('click', async () => { console.log(`you chose ${btn.textContent}, lets see what the computer chose.. `)}));
+      }
 
-      alert(`you chose ${playerSelection}, lets see what the computer chose.. `);
-
-      function computerPlay () { return gameInput[Math.floor(Math.random()*gameInput.length)]; }
+      function getComputerChoice() { return console.log(gameInput[Math.floor(Math.random()*gameInput.length)]);}
       
 
       function playRound(playerSelection, computerSelection) {
+        const gameOutput = ['you won!', 'you lost :( ', 'it\'s a tie!!!!'];
+        const gameOutcome = score => console.log(gameOutput[score]);
         switch(true) {
           case playerSelection === computerSelection :
         gameOutcome(2);
@@ -34,12 +31,13 @@ function game(){
         }
         return winCount;
       }
-    
-      computerPlay();
-      alert(computerSelection);
-      playRound(playerSelection, computerSelection);
+      const playerSelection_btn = getPlayerChoice();
+      const computerSelection = getComputerChoice();
+      playRound(playerSelection_btn, computerSelection);
       counter ++;
-  }
+      console.log(`This was try ${counter} out of 5 total, you won a total of ${winCount}`);
+
+}
 
 game();
 
